2fb6754bc34852e48c9cc435c1f128865e69971e
hizkia15 <hizkiamanurung06@gmail.com>
2024-02-08 14:17:45 +0700

    option pupeeter only no sandbox

--- pages/api/socket.js
+++ pages/api/socket.js
@@ -45,15 +45,7 @@ export default function handler(req, res) {
         authStrategy: new LocalAuth(),
         puppeteer: {
           headless: true,
-          args: [
-            "--no-sandbox",
-            "--disable-setuid-sandbox",
-            "--disable-dev-shm-usage",
-            "--disable-accelerated-2d-canvas",
-            "--no-first-run",
-            "--no-zygote",
-            "--disable-gpu",
-          ],
+          args: ["--no-sandbox"],
         },
       });
 
